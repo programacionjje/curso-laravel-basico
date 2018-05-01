63284b8f825e73391e22c8b0311d485ef755d152
zaratedev <zaratedev@gmail.com>
2018-05-01 17:01:33 -0500

    Creando migracion notas.

---
+++ database/migrations/2018_05_01_214756_create_table_notes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableNotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id'); // primaria
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps(); // created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
