1aaa202a9eb0de9a6c4796116015ed6983e930ed
zaratedev <zaratedev@gmail.com>
2018-06-30 21:01:58 -0500

    Creando vista para mostrar los productos y formulario de crear

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+      return view('products.index');
+    }
+
+    public function create()
+    {
+      return view('products.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,4 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'ProductController@index');
+Route::get('/products/create', 'ProductController@create');
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/usuarios', 'UserController@index');
-Route::get('/usuarios/{id}','UserController@show')->where('id', '[0-9]');
-Route::get('/usuarios/crear', 'UserController@create');
